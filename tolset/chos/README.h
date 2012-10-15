da8c489a98ce8e0dd2babe54bc3653f70f5d7886
ryo <code.red.13@gmail.com>
2012-10-16 01:04:28 +0900

    README.hファイルの追加と，doxygenコメントの追加

---
+++ tolset/chos/README.h
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @brief doxygen用のプロジェクト説明テキスト
+ */
+
+/**
+ * @mainpage
+ * @brief はりぼてOSベースの自作OS
+ *
+ * 自作OS入門の「はりぼてOS」をベースに独自のOSを作る．
+ *
+ * @note http://13.is-a-geek.com/apps/redmine/projects/chos
+ * @note https://github.com/red13/chos
+ */
